a967e8ef26572bade8397066f7e7f1c16390c1be
LAURA BIONDO DE OLIVEIRA <laura.oliveira35@portalsesisp.org.br>
2026-08-12 11:34:54 -0300

    12/08/2026 11:32

--- 01_revisao/09atividade.php
+++ 01_revisao/09atividade.php
@@ -3,15 +3,15 @@ $idade = 15;
 $acompanhado = true; 
 
 if ($idade >= 18) {
-    echo "Entrada liberada! A pessoa pode entrar sozinha.";
+    echo "Entrada liberada! A pessoa pode entrar sozinha.✅";
 } elseif ($idade >= 14 && $idade <= 17) {
     if ($acompanhado) {
-        echo "Entrada liberada! A pessoa está acompanhada por um adulto.";
+        echo "Entrada liberada! A pessoa está acompanhada por um adulto.✅";
     } else {
-        echo "Entrada negada! Menores de 14 a 17 anos precisam estar acompanhados.";
+        echo "Entrada negada! Menores de 14 a 17 anos precisam estar acompanhados.❌";
     }
 } else {
-    echo "Entrada negada! Menores de 14 anos não podem entrar.";
+    echo "Entrada negada! Menores de 14 anos não podem entrar.❌❌❌";
 }
    
 ?>
