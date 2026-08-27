de7f6da21c314d5dc1df280c447817f5df41d96b
LAURA BIONDO DE OLIVEIRA <laura.oliveira35@portalsesisp.org.br>
2026-08-27 15:16:01 -0300

    27/08/2026 15:10

---
+++ 02_funcao/desafio/atividade.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'funcao.php';
+
+$meuPedido = calcularPedido('Teclado', 75.00, 5, 15, 5);
+
+
+echo "Produto: " . $meuPedido['nome'] . "<br>";
+echo "Subtotal: R$ " . $meuPedido['subtotal'] . "<br>";
+echo "Desconto: R$ " . $meuPedido['desconto'] . "<br>";
+echo "Imposto: R$ " . $meuPedido['imposto'] . "<br>";
+echo "Total Final: R$ " . $meuPedido['total'] . "<br>";
+
+$totalComFrete = calcularFrete ($meuPedido['total']);
+echo "Total com frete: " . $totalComFrete
+
+?>
